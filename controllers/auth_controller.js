50d940293ff9112b59d692806eb59e2a67bf519a
vaibhavkumar-07-blast <viranskh336907@gmail.com>
2023-05-17 01:02:34 +0530

    user created with phone number and other details

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -4,12 +4,13 @@ const User = require("../models/user_schema");
 exports.createUser = async function (req, res) {
   try {
     console.log("In auth controller");
-    const { userName, email, password, role } = req.body;
+    const { userName, email, password, role, phoneNumber } = req.body;
     let id = await auth_service.createNewUser({
       userName,
       email,
       password,
       role,
+      phoneNumber,
     });
     res.status(201).send({ message: "successfully created a new User", id });
   } catch (error) {
--- services/auth_service.js
+++ services/auth_service.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 const User = require("../models/user_schema");
-exports.createNewUser = async function (userName, email, password, role) {
+exports.createNewUser = async function (
+  userName,
+  email,
+  password,
+  role,
+  phoneNumber
+) {
   try {
     console.log("In auth service");
-    let user = new User(userName, email, password, role);
+    let user = new User(userName, email, password, role, phoneNumber);
     let result = await user.save();
     const user_id = result._id;
     return user_id;
