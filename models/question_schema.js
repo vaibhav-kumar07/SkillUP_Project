01f348af1484c72a01fdd484e73498a0275f4be3
vaibhavkumar-07-blast <viranskh336907@gmail.com>
2023-05-16 17:14:57 +0530

    question bank added

---
+++ models/question_schema.js
@@ -0,0 +1,45 @@
+const { default: mongoose } = require("mongoose");
+
+const questionSchema = new mongoose.Schema({
+  Title: {
+    type: String,
+    trim: true,
+    required: true,
+    maxlength: 500,
+  },
+  Options: {
+    type: [String],
+    trim: true,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return v.length === 4;
+      },
+      return: { message: "Array must have a length of 4" },
+    },
+  },
+  Answer: {
+    type: String,
+    required: true,
+    enum: [],
+    validate: {
+      validator: function (v) {
+        return this.options.includes(v);
+      },
+      message: "Answer must be one of the options",
+    },
+  },
+  Type: {
+    type: String,
+    trim: true,
+    required: true,
+    lowercase: true,
+    enum: ["easy", "medium", "hard"],
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+module.exports = mongoose.model("Question", questionSchema);
