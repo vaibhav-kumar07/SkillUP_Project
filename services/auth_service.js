daac77841c608063c3f459b2098625806dee5e5c
vaibhavkumar-07-blast <viranskh336907@gmail.com>
2023-05-16 18:47:01 +0530

    user routes added login in service file

--- services/auth_service.js
+++ services/auth_service.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
-
-exports.createNewUser = async function (user) {
+const User = require("../models/user_schema");
+exports.createNewUser = async function (userName, email, password, role) {
   try {
     console.log("In auth service");
+    let user = new User(userName, email, password, role);
     let result = await user.save();
     const user_id = result._id;
     return user_id;
-  } catch (err) {
-    throw { message: "user cannot be created" };
+  } catch (error) {
+    throw { message: `user cannot be created${error.message}` };
   }
 };
