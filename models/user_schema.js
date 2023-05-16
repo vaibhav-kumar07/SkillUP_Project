f7942da8ad621e81e5850b33efb3b2a48a41d0a0
vaibhavkumar-07-blast <viranskh336907@gmail.com>
2023-05-16 16:58:59 +0530

    user schema added

---
+++ models/user_schema.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const UserSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    trim: true,
+    required: true,
+    maxlength: 200,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: true,
+    maxlength: 200,
+    required: true,
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["Admin", "Solver"],
+  },
+  token: {
+    type: String,
+  },
+  otp: {
+    type: String,
+    expires: "5m",
+  },
+  phoneNumber: {
+    type: Number,
+    trim: true,
+    minlength: 10,
+    maxlength: 10,
+  },
+  isActive: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = mongoose.model("User", UserSchema);
