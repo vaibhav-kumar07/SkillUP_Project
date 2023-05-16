d28ad5a934c56f7378c5fd210261e7e7dc8bdc7b
Ankit <2002ankit2002@gmail.com>
2023-05-16 16:23:47 +0530

    initial commit with connection added

--- dbutils/dbutil.js
+++ dbutils/dbutil.js
@@ -1,1 +1,19 @@
+const mongoose = require("mongoose");
 
+const initDB = () => {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("Database connected successfully");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const disconnectDB = () => {
+  mongoose.disconnect();
+  console.log("Database disconnected successfully");
+};
+
+module.exports = { initDB, disconnectDB };
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+require("dotenv").config();
+const dbUtils = require("./dbutils/dbutil");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+dbUtils.initDB();
+
+process.on("SIGINT", () => {
+  dbUtils.disconnectDB();
+  console.log("Closing server");
+  process.exit();
+});
+
+process.on("exit", () => {
+  console.log("Server closed");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port ", PORT);
+});
